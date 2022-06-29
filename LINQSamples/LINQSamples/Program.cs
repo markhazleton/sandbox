de207be4ba3d0db68c51033eb7e9801996a5ac78
Mark Hazleton <mark.hazleton@controlorigins.com>
2022-06-28 21:12:18 -0500

    add LINQSample project

---
+++ LINQSamples/LINQSamples/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using LINQSamples;
+
+var mytest = new CounterList();
+
+var totalCount = mytest.Items.Sum(s => s.TotalCount);
+var unreadCount = mytest.Items.Sum(s => s.UnreadCount);
+
+
+
+
+Console.WriteLine($"You have read {totalCount - unreadCount} of {totalCount} messages");
