ade2507b627d09f8e6a3d41b279a09b36e74b399
Mark Hazleton <mark.hazleton@controlorigins.com>
2023-08-06 18:58:28 -0500

    Concurrent Processing demo

---
+++ ConcurrentProcessing/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+
+const int maxConcurrency = 10; // Maximum concurrent tasks allowed
+
+SemaphoreSlim semaphore = new(maxConcurrency);
+
+List<Task> tasks = new();
+
+int? taskId = 0;
+while (taskId is not null)
+{
+    Task task = ProcessAsync(taskId.Value, tasks.Count, await AwaitSemaphoreAsync(semaphore), semaphore);
+    tasks.Add(task);
+    taskId = GetNextTaskId(taskId);
+
+    if (tasks.Count >= maxConcurrency)
+    {
+        Task finishedTask = await Task.WhenAny(tasks);
+        tasks.Remove(finishedTask);
+    }
+
+}
+await Task.WhenAll(tasks);
+
+static int? GetNextTaskId(int? taskId)
+{
+    if (taskId < 100) return taskId + 1;
+    else return null;
+}
+
+static async Task ProcessAsync(int taskId, int taskCount, long waitTicks, SemaphoreSlim semaphore)
+{
+    try
+    {
+        await Task.Delay(TimeSpan.FromMilliseconds(new Random().Next(1, 5000)));
+        Console.WriteLine($"Task:{taskId:D3} T:{taskCount} S:{semaphore.CurrentCount} W:{waitTicks}");
+    }
+    finally
+    {
+        semaphore.Release();
+    }
+}
+
+static async Task<long> AwaitSemaphoreAsync(SemaphoreSlim semaphore)
+{
+    Stopwatch stopwatch = Stopwatch.StartNew();
+    await semaphore.WaitAsync();
+    stopwatch.Stop();
+    return stopwatch.ElapsedTicks;
+}
