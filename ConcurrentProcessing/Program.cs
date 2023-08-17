48ab46207bcef2fe4e523bffab0f41c2fd261c0c
Mark Hazleton <mark.hazleton@controlorigins.com>
2023-08-17 09:48:15 -0500

    Code clean-up, add const for max number of tasks

--- ConcurrentProcessing/Program.cs
+++ ConcurrentProcessing/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System.Diagnostics;
 
-const int maxConcurrency = 10; // Maximum concurrent tasks allowed
+const int maxTaskCount = 10; // Maximum total tasks allowed
+const int maxConcurrency = 2; // Maximum concurrent tasks allowed
 
 SemaphoreSlim semaphore = new(maxConcurrency);
-
 List<Task> tasks = new();
-
 int? taskId = 0;
 while (taskId is not null)
 {
@@ -18,13 +17,12 @@ while (taskId is not null)
         Task finishedTask = await Task.WhenAny(tasks);
         tasks.Remove(finishedTask);
     }
-
 }
 await Task.WhenAll(tasks);
 
 static int? GetNextTaskId(int? taskId)
 {
-    if (taskId < 100) return taskId + 1;
+    if (taskId < maxTaskCount) return taskId + 1;
     else return null;
 }
 
@@ -32,7 +30,7 @@ static async Task ProcessAsync(int taskId, int taskCount, long waitTicks, Semaph
 {
     try
     {
-        await Task.Delay(TimeSpan.FromMilliseconds(new Random().Next(1, 5000)));
+        await Task.Delay(TimeSpan.FromMilliseconds(new Random().Next(1, 500)));
         Console.WriteLine($"Task:{taskId:D3} T:{taskCount} S:{semaphore.CurrentCount} W:{waitTicks}");
     }
     finally
