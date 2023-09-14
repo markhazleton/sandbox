0dc3e23c71adad0dfe045ed5d163260a48399af3
Mark Hazleton <mark.hazleton@controlorigins.com>
2023-09-14 13:08:29 -0500

    Revert "remove ConcurrentProcessing to HttpClientDecorator Project"
    This reverts commit df174d0be455530c68a51baa57bd3a8e4524ffbd.

---
+++ ConcurrentProcessing/Concurrent/ConcurrentProcessorModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ConcurrentProcessing.Concurrent;
+
+public class ConcurrentProcessorModel
+{
+    public int TaskId { get; set; }
+    public int TaskCount { get; set; }
+    public long WaitMS { get; set; }
+    public int SemaphoreCount { get; set; }
+    public long SemaphoreWait { get; set; }
+    public override string? ToString()
+    {
+        return $"Task:{TaskId:D3} Duration:{WaitMS:D5} TaskList:{TaskCount:D2} SemaphoreCount:{SemaphoreCount:D2} SemaphoreWait:{SemaphoreWait:D4}";
+    }
+
+}
+
---
+++ ConcurrentProcessing/MyTask.cs
@@ -0,0 +1,17 @@
+﻿using ConcurrentProcessing.Concurrent;
+
+class MyTask : ConcurrentProcessorModel
+{
+    public MyTask(ConcurrentProcessorModel model)
+    {
+        TaskId = model.TaskId;
+        TaskCount = model.TaskCount;
+        WaitMS = model.WaitMS;
+        SemaphoreCount = model.SemaphoreCount;
+        SemaphoreWait = model.SemaphoreWait;
+    }
+
+}
+
+
+
